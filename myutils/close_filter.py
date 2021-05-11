ed50bb72b08f38c73d8fc4f8271b8f44c3ed21ef
kmint <kmint1819@gmail.com>
2021-05-12 01:07:00 +0800

    myutils

--- myutils/close_filter.py
+++
@@ -1,4 +0,0 @@
-import cv2 
-import numpy as np
-
-def filter_close_points(arr)
---
+++ myutils/filter.py
@@ -0,0 +1,8 @@
+import cv2 
+import numpy as np
+
+border_width = 20
+tolerance = 5
+
+def filter_close_points(arr):
+    
